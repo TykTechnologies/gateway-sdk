89845c0b99752a8a816322d9b661f46ed0c9d51e
keith wachira <keithwachira@gmail.com>
2023-01-06 15:29:12 +0300

    change policy _id to MID

--- examples/main.go
+++ examples/main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"context"
-	"github.com/TykTechnologies/gateway-sdk/gateway"
-
-	"log"
-)
-
-var (
-	baseUrl = "http://localhost:8080"
-)
-
 func main() {
-	conf := gateway.Configuration{
-		BasePath:      baseUrl,
-		Host:          "",
-		Scheme:        "",
-		DefaultHeader: map[string]string{},
-		UserAgent:     "",
-		HTTPClient:    nil,
-	}
-	client := gateway.NewAPIClient(&conf)
-	ctx := context.Background()
-	api, resp, err := client.HealthCheckingApi.HelloAPI(ctx, "hello")
-	if err != nil {
-		panic(err)
-	}
-	if resp.StatusCode != 200 {
-		panic(resp.StatusCode)
-	}
-	log.Println(api)
 
 }
