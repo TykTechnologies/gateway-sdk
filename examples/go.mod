c631908efa375633d4da5d5ed919104a3c3447dc
keith wachira <keithwachira@gmail.com>
2023-03-06 10:06:51 +0300

    add example to the sdks

---
+++ examples/go.mod
@@ -0,0 +1,5 @@
+module examples
+
+go 1.19
+
+require github.com/TykTechnologies/gateway-sdk v0.0.1
--- examples/policy.go
+++ examples/policy.go
@@ -1,20 +1,3 @@
-# Gateway SDK ( Alpha )
-
-- This is the gateway sdk generated from the open Api specs.
-- Report any issue you may encounter will using this Sdk as we plan to perfect them before making them public.
-
-## How to install
-
-To install the sdk run
-
-`go get github.com/TykTechnologies/gateway-sdk`
-
-## Sample Usage
-In these samples Tyk Gateway is running on localhost port 8080.
-The BaseUrl is: `http://localhost:8080` and the secret key is `secret`, you should change this to your actual key.
-
-## List Policies
-```go
 package main
 
 import (
@@ -58,8 +41,3 @@ func main() {
 	}
 	fmt.Printf("%+v\n", policies)
 }
-```
-
-## Documentation
-
-For documentation please [look here](https://github.com/TykTechnologies/gateway-sdk/blob/main/gateway/README.md).
